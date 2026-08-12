2705da7c5175c3276085154ff9cb8f7959fa46d6
Harsh Singh <harsh.singh0607@gmail.com>
2026-08-13 00:04:26 +0530

    feat(d2): variables, reassignment and type() - D2 lab

---
+++ labs/2026-08-12/profile.py
@@ -0,0 +1,11 @@
+full_name = "Harsh Kumar Singh"
+year_of_experience = 12
+current_city = "Noida"
+print(full_name)
+print(year_of_experience)
+print(current_city)
+
+print(type(year_of_experience))
+
+year_of_experience = year_of_experience + 1
+print(year_of_experience)
---
+++ labs/2026-08-12/variables.py
@@ -0,0 +1,28 @@
+#city = "Pune"
+#print(city)
+
+#score = 10
+#print(score)
+#score = 25
+#print(score)
+
+#print(type(score))
+#print(type(city))
+
+#city = "Pune"
+#print(type(city))
+#city = 42
+#print(type(city))
+
+#x = 5
+#y = x
+#x = "hello"
+#print(y)
+#print(type(y))
+
+
+visits = 0
+visits = visits + 1
+visits = visits + 1
+print(visits)
+print(type(visits))
